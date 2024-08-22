9986db696b058ad5c8873a4f38e6e5f627a85576
Xuanlin <xuanlinamo@berkeley.edu>
2024-08-22 13:23:06 -0700

    added annotation

--- newton.py
+++ newton.py
@@ -1,10 +1,28 @@
 def diff(f,x,sig=1e-4):
+    """first-order derivative of function `f` at point `x`
+    ---
+    * f: target function
+    * x: point
+    * sig: neighbor range of x
+    """
     return (f(x+sig/2)-f(x-sig/2))/sig
 
 def diff2(f,x,sig=1e-4):
+    """second-order derivative of function `f` at point `x`
+    ---
+    * f: target function
+    * x: point
+    * sig: neighbor range of x
+    """
     return (diff(f,x+sig/2) - diff(f,x-sig/2))/sig
 
 def Newton(x0,f,error=1e-4):
+    """Newton method optimization
+    ---
+    * x0: start point
+    * f: target function
+    * error: absolute error of optimization
+    """
     delta = float('inf')
     x = x0
     while delta>error:
